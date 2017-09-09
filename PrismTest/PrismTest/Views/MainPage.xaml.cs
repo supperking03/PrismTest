f8dd593db4d6a8c157f92bf847178dd4ca6e0f16
Khanh dep trai <ijuinenzan@yahoo.com>
2017-09-09 10:56:21 +0700

    Add project files.

---
+++ PrismTest/PrismTest/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismTest.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
